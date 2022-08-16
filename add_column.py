a2ff1e77ad9dd5769fb71a6d8200ad6ab5292d5b
rosenpin <mail@tomerrosenfeld.com>
2022-08-16 21:00:28 +0300

    updated dataset

---
+++ add_column.py
@@ -0,0 +1,19 @@
+
+import csv
+
+
+to_remove = []
+with open("dataset.csv") as fp:
+    reader = csv.reader(fp, delimiter=",", quotechar='"')
+    # next(reader, None)  # skip the headers
+    data_read = [row for row in reader]
+    for i in range(len(data_read)):
+        if (i ==0):
+            continue
+        new_line = data_read[i] 
+        new_line.append("{:.4f}".format(int(new_line[5])/int(new_line[6])))
+        data_read[i] = new_line
+
+    with open("newdataset.csv","w+") as newcsv:
+        writer = csv.writer(newcsv, delimiter=",")
+        writer.writerows(data_read)
