00d06cfd6a3e84b55159ea6550d565577cf45911
ISHIBubble <101247478+ISHIBubble@users.noreply.github.com>
2022-08-17 12:17:04 +0300

    Update 206836009 – 318862398.R

--- 206836009 – 318862398.R
+++ 206836009 – 318862398.R
@@ -1,5 +1,23 @@
 library(ggplot2)
-dataset = read.csv(file = "dataset.csv")
+library(car)
+library(ggpubr)
+dataset = read.csv(file = "206836009 – 318862398.csv")
 
-qqplot(dataset$Followers, )
+countries <- c("Brazil", "India", "Indonesia", "United States")
+#creating a factor defining groups vector
+groupings <- dataset$Audience.country.mostly.[dataset$Audience.country.mostly. %in% countries]
+#creating the ordered values vector
+values <- dataset$Authentic.engagement[dataset$Audience.country.mostly. %in% countries]
+#combining into dataframe
+data.for.test <- data.frame(values,groupings)
+
+##Testing for normality
+#Shapiro-wilk test
+shapiro.test(values)
+
+#qqplot test
+ggqqplot(values)
+
+#preforming Levene test (F test for multiple groups)
+leveneTest(y = values, group = groupings, data = data.for.test, center = mean)
 
