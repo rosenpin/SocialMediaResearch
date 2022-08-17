0e9b483797f892abca5ac7f84cae1c64fbdb4e01
rosenpin <mail@tomerrosenfeld.com>
2022-08-17 10:16:11 +0300

    updated tomer script

--- tomer.R
+++ tomer.R
@@ -1,5 +1,17 @@
 library(ggplot2)
-dataset = read.csv(file = "dataset.csv")
+dataset = read.csv(file = "newdataset.csv")
 
-ratio <- dataset$Engagement.ratio
-hist(dnorm(ratio))
+##### Exercise 1 #####
+india_only <- dataset[dataset$Audience.country.mostly. == 'India', ]
+india_only_cinema_category <- india_only[india_only$category_1 == "Cinema & Actors/actresses",]
+p_indians <- nrow(india_only_cinema_category)
+total_indians <- nrow(india_only)
+
+general_population_cinema_category <- dataset[dataset$category_1 == "Cinema & Actors/actresses",]
+p_population <- nrow(general_population_cinema_category)
+total_general_population <- nrow(dataset)
+
+prop.test(c(p_indians, p_population),
+          c(total_indians, total_general_population),
+          alternative = c("two.sided"),
+          conf.level = 0.95)
