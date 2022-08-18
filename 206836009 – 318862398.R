d7f0a384591f92aa7b4b13e082c0f40cb73247bc
ISHIBubble <101247478+ISHIBubble@users.noreply.github.com>
2022-08-18 03:30:50 +0300

    Update 206836009 – 318862398.R

--- 206836009 – 318862398.R
+++ 206836009 – 318862398.R
@@ -33,22 +33,21 @@ bartlett.test(values ~ groupings, data=data.for.test)
 ###  Preforming Levene test (F test for multiple groups)
 leveneTest(values, as.factor(groupings), data = data.for.test, center = mean)
 
-
 ####      Question Three
+
 #finding mean followers count
-music_mean_followers <- mean(dataset$Followers)
+mean_music_followers <- mean(dataset$Followers)
 #Getting engagement ratios for bigger and smaller accounts
-bigger_music_ratio <- dataset$Engagement.ratio[dataset$category_1 == "Music" & dataset$Followers > music_mean_followers]
-smaller_music_ratio <- dataset$Engagement.ratio[dataset$category_1 == "Music" & dataset$Followers <= music_mean_followers]
-#Calculating standard deviation for both groups
-sd_bigger <- sd(bigger_music_ratio)
-sd_smaller <- sd(smaller_music_ratio)
-sd_est <- sqrt((sd_bigger*length(bigger_music_ratio)+sd_smaller*length(smaller_music_ratio))/(length(smaller_music_ratio)+length(bigger_music_ratio)-2))
-se_est <- sd_est*sqrt(1/length(bigger_music_ratio)+1/length(smaller_music_ratio))
-print(se_est)
+ratio_music_bigger <- dataset$Engagement.ratio[dataset$category_1 == "Music" & dataset$Followers > mean_music_followers]
+ratio_music_smaller <- dataset$Engagement.ratio[dataset$category_1 == "Music" & dataset$Followers <= mean_music_followers]
+#Calculating standard deviation for both groups and estimating standard error
+sd_bigger <- sd(ratio_music_bigger)
+sd_smaller <- sd(ratio_music_smaller)
+sd_est <- sqrt((sd_bigger*length(ratio_music_bigger)+sd_smaller*length(ratio_music_smaller))/(length(ratio_music_smaller)+length(ratio_music_bigger)-2))
+se_est <- sd_est*sqrt(1/length(ratio_music_bigger)+1/length(ratio_music_smaller))
 #Creating dataframe for tests
-a <- data.frame(values = bigger_music_ratio, groups = rep("Big", length(bigger_music_ratio)))
-b <- data.frame(values = smaller_music_ratio,groups = rep("Small", length(smaller_music_ratio)))
+a <- data.frame(values = ratio_music_bigger, groups = rep("Big", length(ratio_music_bigger)))
+b <- data.frame(values = ratio_music_smaller,groups = rep("Small", length(ratio_music_smaller)))
 data.for.test2 <- rbind(a,b)
 
 ###  Testing assumptions
@@ -68,7 +67,14 @@ hist(data.for.test2$values, breaks = 80)
 #Shapiro-wilk test
 shapiro.test(data.for.test2$values)
 
-
 ### Preforming T test
 qt(.95,232)
-t.test(bigger_music_ratio, smaller_music_ratio, var.equal = TRUE)
+t.test(ratio_music_bigger, ratio_music_smaller, var.equal = TRUE)
+
+### Calculating Cohen's D
+#finding mean values for both groups
+mean_bigger <- mean(ratio_music_bigger)
+mean_smaller <- mean(ratio_music_smaller)
+#calculating d
+cohens_d <- (mean_bigger - mean_smaller)/se_est
+print(cohens_d)
